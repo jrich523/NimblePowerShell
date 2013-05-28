63840d6dadc82e6cc3e285279fa5000df2f2bab6
jrich523 <jrich523@gmail.com>
2013-05-28 16:22:36 -0700

    added initiator.ps1 for loading

--- Nimble.psm1
+++ Nimble.psm1
@@ -5,6 +5,7 @@
 . $psScriptRoot\array.ps1
 . $psScriptRoot\perfpolicy.ps1
 . $psScriptRoot\snap.ps1
+. $psScriptRoot\Initiator.ps1
 
 $myInvocation.MyCommand.ScriptBlock.Module.OnRemove = { 
     Clear-FormatData
