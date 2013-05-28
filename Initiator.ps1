d4452c25edfd973eb9f383677a0a3bfa139f2024
jrich523 <jrich523@gmail.com>
2013-05-28 16:43:38 -0700

    add-nsinitiatortogroup added

--- Initiator.ps1
+++ Initiator.ps1
@@ -35,6 +35,10 @@ function Get-NSInitiatorGroup
     }
     Process
     {
+        
+        ##todo:
+        ##process date
+        #[TimeZone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($igrp.creationtime))
         if($name)
         {
             $rtnigrp += $igrp | where {$_.name -like $name}
@@ -47,5 +51,54 @@ function Get-NSInitiatorGroup
     End
     {
         $rtnigrp
+    }
+}
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Add-NSInitiatorToGroup
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(ValueFromPipeline=$true,Position=0)]
+        $Name,
+        # group name
+        [Parameter(ValueFromPipelineByPropertyName=$true,Position=1)]
+        $InitiatorGroup
+    )
+
+    Begin
+    {
+        if(-not $Script:NSUnit)
+        {
+            Write-Error "Connect to unit first!" -ErrorAction Stop
+        }
+            
+    }
+    Process
+    {
+        foreach($init in $Name)
+        {
+            $i = New-Object initiator
+            $i.name = $init
+            $rtncode = $Script:NSUnit.addInitiatort($sid.Value,$InitiatorGroup, $i)
+            if($rtncode -ne "Smok")
+            {
+                Write-Error "Error adding $init to the list! code: $rtncode"
+            }
+        }
+    }
+    End
+    {
     }
 }
