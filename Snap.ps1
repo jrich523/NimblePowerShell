7edf6c9e0f6fd1a7353b7ca956690167db24271e
jrich523 <jrich523@gmail.com>
2013-05-31 10:17:49 -0700

    cleanup and function addition (new-nsclone in progress)

--- Snap.ps1
+++ Snap.ps1
@@ -167,3 +167,123 @@ function Remove-NSSnapShot
 
     }
 }
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function New-NSClone
+{
+    [CmdletBinding()]
+    Param
+    (
+        # can only contain letters,numbers,dash,dot - write regex
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ValueFromPipeline=$true,
+                   Position=1)]
+        [ValidatePattern('^[a-z,A-Z,\d,\.,-]+$')]
+        [string]
+        $Name,
+
+        # Param2 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=2)]
+        
+        $Size,
+
+        #desc
+        [string]
+        $Description,
+
+        #more
+        [switch]
+        $MultipleInitiator,
+        
+        #details
+        [ValidateRange(0,100)]
+        [int]
+        $VolumeReserve=0,
+
+        #details
+        [ValidateRange(0,100)]
+        [int]
+        $VolumeQuote=100,
+
+        #details
+        [ValidateRange(0,100)]
+        [int]
+        $VolumeWarning=80,
+
+        #details
+        [ValidateRange(0,100)]
+        [int]
+        $SnapShotReserve=0,
+
+        #details - -1 means unlimited
+        [ValidateRange(-1,100)]
+        [int]
+        $SnapShotQuote=-1,
+
+        #details
+        [ValidateRange(0,100)]
+        [int]
+        $SnapShotWarning=0
+
+    )
+    DynamicParam {
+
+        New-DynamicParam -Name PerformancePolicy -Options (Get-NSPerfPolicy | select -ExpandProperty name) -Manditory -Position 3
+    }
+    Begin
+    {
+        $attr = New-Object VolCreateAttr
+        $attr.size = $Size
+        #vol prop
+        $attr.warnlevel = $Size * ($VolumeWarning /100)
+        $attr.quota = $Size * ($VolumeQuote/100)
+        $attr.reserve = $Size * ($VolumeReserve/100)
+        #snap prop
+        if($SnapShotQuote -eq -1)
+        {
+            $attr.snapquota = 9223372036854775807  ##unlimited
+        }
+        else
+        {
+            $attr.snapquota = $size * ($SnapShotQuote /100)
+        }
+        $attr.snapreserve = $Size * ($SnapShotReserve/100)
+        $attr.snapwarnlevel = $size * ($SnapShotWarning/100)
+        #gen prop
+        $attr.description = $Description
+        $attr.online = $true
+        $attr.perfpolname = $PSBoundParameters.PerformancePolicy
+        
+        if($MultipleInitiator)
+        {
+            $attr.multiinitiator = $true
+        }
+        
+
+    }
+    Process
+    {
+        $attr.name = $Name
+        $rtncode = $script:nsunit.createVol($script:sid.Value,$attr,[ref]$str)
+        if($rtncode -ne "Smok")
+        {
+            Write-Error "Error Creating volume $Name! code: $rtncode"
+        }
+        Get-NSVolume $Name
+
+    }
+    End
+    {
+    }
+}
