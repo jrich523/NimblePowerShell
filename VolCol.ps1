271dab9f05ac96a87910519b65e44c2e3b12c0b9
jrich523 <jrich523@gmail.com>
2014-03-19 14:49:54 -0700

    volume collection functions

---
+++ VolCol.ps1
@@ -0,0 +1,128 @@
+﻿## cmdlets to create
+## new-nsvolumecollection
+## get-  probably need format file for it
+## remove-
+## add-* might be a couple needed
+
+
+
+#######################################################
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-NSVolumeCollection
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(ValueFromPipeline=$true,Position=0)]
+        $Name = "*"
+    )
+
+Begin
+    {
+        if(-not $Script:NSUnit)
+        {
+            Write-Error "Connect to unit first!" -ErrorAction Stop
+        }
+        $ppl = new-object protectionpolicy 
+        $rtncode = $Script:NSUnit.getProtPolList($sid.Value, [ref]$ppl)
+        if($rtncode -ne "Smok")
+        {
+            Write-Error "Error getting volume collection list! code: $rtncode" -ErrorAction Stop
+        }
+            
+    }
+    Process
+    {
+        $ppl | where {$_.name -like $name}
+    }
+    End
+    {
+        
+    }
+}
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Remove-NSVolumeCollection
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Param1,
+
+        # Param2 help description
+        [int]
+        $Param2
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function New-NSVolumeCollection
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Param1,
+
+        # Param2 help description
+        [int]
+        $Param2
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+    }
+    End
+    {
+    }
+}
